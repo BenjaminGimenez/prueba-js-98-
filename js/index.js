132bf9f2cefc12ac02a7cf12b1be27c353344722
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-03-19 11:51:44 -0300

    primer commit de los ejercicios de js

---
+++ js/index.js
@@ -0,0 +1,57 @@
+'use strict' //nos permite detectar errores en la consola
+
+console.log('hola mundo')
+
+//VARIABLES
+
+let nombre = 'ezequiel'  //texto
+
+console.log(nombre)
+
+let miNumero = '10'  //numero
+
+console.log(miNumero)
+
+//ALERT
+
+//alert ('bienvenido a nuestro sitio ' + nombre )
+
+//PROPMT
+
+// let respuesta = prompt('ingrese su nombre y apeliido')
+// console.log(respuesta)
+
+//OPERADORES RELACIONALES
+
+// CONDICIONALES
+
+//IF Y ELSE 
+
+//Si es mayor que 2
+if (5<2) {
+    //si se cumple se ejecuta
+    console.log('si se ejecuto')
+
+} else { //SINO
+
+    //se ejecutara lo siguiente
+    console.log('llegamos al else')
+}
+
+//IF ELSE
+
+if (5<2) {
+    //si se cumple se ejecuta
+    console.log('si se ejecuto')
+
+} else  if (15 > 10) { //SINO
+
+    //se ejecutara lo siguiente
+    console.log('llegamos al if else')
+} else {
+    //si la anterior condicion no se cumple entonces se ejecutara 
+    console.log('llegamos al ultimo else')
+}
+
+document.write('hola mundooooo!')
+
